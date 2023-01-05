7156ff07be80cbda2cd68b9d7863fbdef4360765
awaffle01 <95768353+aarav-malik@users.noreply.github.com>
2023-01-06 00:51:10 +0800

    Progress screen updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 
-function placeHolder(){
-  return(
-    <h1>In progress</h1>
-  )
+
+function InProgressScreen() {
+  return (
+    <div className="in-progress-container">
+      <h1>In Progress</h1>
+      <p>This website is currently being built</p>
+      <div className="loading-spinner" />
+    </div>
+  );
 }
 
-export default placeHolder
+export default InProgressScreen;
