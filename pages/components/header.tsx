10c9d023871b0199a1bf38a6ce67d83e60864c82
awaffle01 <95768353+aarav-malik@users.noreply.github.com>
2023-01-21 01:29:32 +0800

    header image

--- pages/components/header.tsx
+++ pages/components/header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Header = () => {
   return (
     <div>
-        <Image src='/images/Header.jpg' alt='Image by Ameer Basheer on Unsplash' width={750} height={250} layout='responsive'/>   
+        <Image src='/images/Header.jpg' alt='Image by Venti Views on Unsplash' width={750} height={250} layout='responsive'/>   
     </div>
   )
 }
