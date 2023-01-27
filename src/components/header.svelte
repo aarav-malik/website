11cd59c68a9ab3c4cc1884f170ac0306fb2f8272
awaffle01 <95768353+aarav-malik@users.noreply.github.com>
2023-01-28 00:49:34 +0800

    Header update
    Title
    Custom cursor
    'Scroll down' icon

---
+++ src/components/header.svelte
@@ -0,0 +1,7 @@
+<script>
+    import '/src/styles.css';
+</script>
+
+<h1 class={`ailerons title`}>Aarav Malik</h1>
+
+<div class="ca3-scroll-down-link ca3-scroll-down-arrow" data-ca3_iconfont="ETmodules" data-ca3_icon=""></div>
