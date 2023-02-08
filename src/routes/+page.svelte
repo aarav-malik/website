4f64c4aa6ea5c5695873b90930d90d0acc01241c
awaffle01 <95768353+aarav-malik@users.noreply.github.com>
2023-02-08 13:10:32 +0800

    header 50% complete

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,13 @@
 <script>
 import Header from "../components/header.svelte";
+import Hbody from "../components/hbody.svelte";
 import '/src/styles.css';
 </script>
 
 <div class="header">
 <Header/>
+</div>
+
+<div>
+    <Hbody/>
 </div>
