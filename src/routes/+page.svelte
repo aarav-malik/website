7b137fb14bb761fef4ae09848c04d66e20aa6fef
awaffle01 <95768353+aarav-malik@users.noreply.github.com>
2023-02-12 23:15:23 +0800

    Update to Svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import '/src/styles/styles.css';
+</script>
+
+<div class="in-progress-container">
+    <h1>In Progress</h1>
+    <p>This website is currently being built</p>
+    <div class="loading-spinner" />
+</div>
