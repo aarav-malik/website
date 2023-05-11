47d7d0612b865f6ab5d219b27ee67e1c8323b833
awaffle01 <95768353+aarav-malik@users.noreply.github.com>
2023-05-11 23:38:30 +0800

    navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,7 @@ import Header from "../components/header.svelte";
 import Hbody from "../components/hbody.svelte";
 import '/src/styles.css';
 </script>
-
-
+  
 <div>
 <title>Hello</title>
 <Header/>
