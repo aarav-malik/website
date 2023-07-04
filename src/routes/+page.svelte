2d7a5b7ad340745c9289056ca772171a616e517f
awaffle01 <95768353+aarav-malik@users.noreply.github.com>
2023-07-04 09:41:21 +0800

    clear

--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import '/src/styles/styles.css';
-</script>
-
-<div class="in-progress-container">
-    <h1>In Progress</h1>
-    <p>This website is currently being built</p>
-    <div class="loading-spinner" />
-</div>
